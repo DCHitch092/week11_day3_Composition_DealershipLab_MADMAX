3fe6522451e5fbf4e8d0f4fc5e98bdc3e61cc14f
Richard <fahitch+github@gmail.com>
2020-01-15 14:16:48 +0000

    add tests to CustomerTest

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+
+    Customer customer;
+    Vehicle vehicle;
+
+    @Before
+    public void before(){
+        customer = new Customer("Furiosa", "Imperator", 25);
+        vehicle = new Vehicle();
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Furiosa", customer.getName());
+    }
+
+    @Test
+    public void hasRank(){
+        assertEquals("Imperator", customer.getRank());
+    }
+
+    @Test
+    public void hasLoot(){
+        assertEquals(25, customer.getLoot());
+    }
+
+    @Test
+    public void hasFleet(){
+        assertEquals(0, customer.getFleet().size());
+    }
+
+    @Test
+    public void canAddToFleet(){
+        customer.addToFleet(vehicle);
+        assertEquals(1, customer.getFleet().size());
+    }
+
 }
