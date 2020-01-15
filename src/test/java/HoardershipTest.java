af7e55c213f6033ad9dea407052c878c13b2a7fe
Richard <fahitch+github@gmail.com>
2020-01-15 14:06:51 +0000

    adds tests to Hoardership

--- src/test/java/HoardershipTest.java
+++ src/test/java/HoardershipTest.java
@@ -1,2 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HoardershipTest {
+
+    Hoardership hoardership;
+    Vehicle vehicle;
+
+    @Before
+    public void before(){
+        hoardership = new Hoardership("The Citadel");
+        vehicle = new Vehicle();
+    }
+
+    @Test
+    public void hasStock(){
+        assertEquals(0, hoardership.getStock().size());
+    }
+
+    @Test
+    public void canAddStock(){
+        hoardership.addStock(vehicle);
+        assertEquals(1, hoardership.getStock().size());
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("The Citadel", hoardership.getName());
+    }
+
+    @Test
+    public void hasWarChest(){
+        assertEquals(10000, hoardership.getWarChest(), 0.01 );
+    }
+
 }
